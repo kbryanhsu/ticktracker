cb8d89e9124b3477e8c33a9b0b1fcef2f078d416
Kenneth Bryan Hsu <bryanhsu2020@berkeley.edu>
2021-03-20 16:03:30 -0700

    works for all files in a given directory

--- sad_cat.py
+++ sad_cat.py
@@ -1,23 +1,27 @@
 import os
 import csv
 
-with open('AAL.csv', newline='') as sourceFile:
-    with open('AALoutput.csv', mode = 'w', newline='') as outputFile:
-        reader = csv.reader(sourceFile, delimiter =' ', quotechar ='|')
-        writer = csv.writer(outputFile, quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-        first = 1   
-        last = 0
-        currentDate = 0
-        firstRowCheck = True
-        for row in reader:
-            if firstRowCheck:
-                firstRowCheck = False
-                continue
-            if row[0] != currentDate:
-                writer.writerow([currentDate, ((int(last)-int(first))/int(first)) * 100])
-                currentDate = row[0]
-                firstindex = row[1].index(',')
-                first = row[1][firstindex+1:]
-            lastindex = row[1].index(',')
-            last = row[1][lastindex+1:]
+files = os.scandir()
 
+for f in files:
+    if '.csv' not in f.name or 'output' in f.name:
+        continue
+    firstRowCheck = True
+    with open(f.name, newline='') as sourceFile:
+        with open(f.name+'output.csv', mode = 'w', newline='') as outputFile:
+            reader = csv.reader(sourceFile, delimiter =' ', quotechar ='|')
+            writer = csv.writer(outputFile, quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+            first = 1   
+            last = 0
+            currentDate = 0
+            for row in reader:
+                if firstRowCheck:
+                    firstRowCheck = False
+                    continue
+                if row[0] != currentDate:
+                    writer.writerow([currentDate, ((int(last)-int(first))/int(first)) * 100])
+                    currentDate = row[0]
+                    firstindex = row[1].index(',')
+                    first = row[1][firstindex+1:]
+                lastindex = row[1].index(',')
+                last = row[1][lastindex+1:]
